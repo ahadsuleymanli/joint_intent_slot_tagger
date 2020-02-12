aed54d3c9a037613cc6306051974896cefe9c51b
ahadsuleymanli <ahadsuleymanli@gmail.com>
2020-02-13 00:59:17 +0300

    cleaned up and added comments

--- mysite/submit_intents/create_dataset.py
+++ mysite/submit_intents/create_dataset.py
@@ -1,3 +1,6 @@
+'''
+    exports dataset from the DB into a folder containing label, seq.in, seq.out text files 
+'''
 from .models import IntentInstance
 import os
 
@@ -5,7 +8,6 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATASET_DIR = os.path.join(ROOT_DIR, 'dataset')
 if not os.path.exists(DATASET_DIR):
     os.makedirs(DATASET_DIR)
-
 
 def get_output_file_paths():
     return (os.path.join(DATASET_DIR, 'label'), 
@@ -17,6 +19,10 @@ class CreateDataset:
 
     @staticmethod
     def add_to_intents_dict(key,value):
+        '''
+            if key doesnt exist in the dictionary, create
+            if exists append the value to the key's array
+        '''
         if key not in CreateDataset.intents_dict:
             CreateDataset.intents_dict[key] = [value,]
         else:
@@ -24,20 +30,24 @@ class CreateDataset:
     
     @staticmethod
     def create_dataset():
+        '''
+            outputs the dataset in the form of label, seq.in, seq.out text files
+            each file contains one line per entry in dataset
+        '''
         label_path, seq_in_path, seq_out_path = get_output_file_paths()
         label_file = open(label_path,"w+")
         seq_in_file = open(seq_in_path,"w+")
         seq_out_file = open(seq_out_path,"w+")
 
+        # getting all submitted intents from DB in random order
         intents = IntentInstance.objects.order_by('?')
-
-        # getting unique labels
-        # unique_labels = IntentInstance.objects.order_by().values_list('label',flat=True).distinct()
-        # unique_labels = list(unique_labels)
+        
+        # creating a dictionary of intents labels mapped to an array of (seq_in, seq_out) tuples  
         for intent in intents:
-            CreateDataset.add_to_intents_dict(intent.label,(intent.seq_in,intent.seq_out))
+            CreateDataset.add_to_intents_dict(intent.label, (intent.seq_in,intent.seq_out))
 
-
+        # iterating over the dictionry Breadth-First 
+        # in order to output one of each intent category alternatingly into the output files
         while not all(len(CreateDataset.intents_dict[key])==0 for key in CreateDataset.intents_dict):
             for key in CreateDataset.intents_dict:
                 if len(CreateDataset.intents_dict[key]):
@@ -45,12 +55,6 @@ class CreateDataset:
                     label_file.write(key+'\n')
                     seq_in_file.write(seq_in+'\n')
                     seq_out_file.write(seq_out+'\n')
-
-        
-        print(CreateDataset.intents_dict)
-        # for key in CreateDataset.intents_dict:
-        # for intent in intents:
-        #     print (intent.label)
 
         label_file.close()
         seq_in_file.close()
