9e767c7a25930af112bc8fee49abd1e916f6b2b6
ahadsuleymanli <ahadsuleymanli@gmail.com>
2020-06-15 13:00:59 +0300

    implemented augmentable datased copy constructor

--- mysite/submit_intents/dataset_augmentation/dataset.py
+++ mysite/submit_intents/dataset_augmentation/dataset.py
@@ -1,108 +1,148 @@
 import numpy as np 
 import pandas as pd 
-from .utils import group_words_into_slots
 from ..models import IntentInstance, IntentCategory
-from .utils import goo_to_dataframe, dataframe_to_goo
+from .utils import goo_to_dataframe, dataframe_to_goo, get_augmentation_settings
+from copy import deepcopy
+'''
+#### augmentation_setting dataframes are of the form:
+SendEmail                EXCEMPT_STEMM  EXCEMPT_SYNON EXCEMPT_SHUFF UNIQUE_VALUES
+SLOT                                                                   
+empty_message            NaN            NaN           NaN           NaN
+empty_subject            NaN            NaN           NaN           NaN
+message                  NaN            NaN          True          True
+message_end              NaN            NaN          True           NaN
+message_start            NaN            NaN          True           NaN
+name                     NaN            NaN           NaN           NaN
+send_email               NaN            NaN           NaN           NaN
+subject                  NaN            NaN          True          True
+subject_end              NaN            NaN          True           NaN
+subject_start            NaN            NaN          True           NaN
 
-class Intent:
-    '''
-        represents one datum
-        the datum is represented as a dataframe
-    '''
-    def __init__(self,seq_in,seq_out,label,augmentation_settings_df):
-        '''
-            seq_in: input string
-            seq_out: ground truth of slots
-            label: ground truth value of the category
-        '''
-        seq_in_list, seq_out_list = group_words_into_slots(seq_in,seq_out)
-        self.df = pd.DataFrame({"TOKEN":np.array(seq_in_list),"SLOT":np.array(seq_out_list)})
-    def get_strings(self):
-        '''
-            returns seq_in, seq_out, label
-        '''
-        pass
+#### intent dataframes are of the form:
+                            TOKEN           SLOT  EXCEMPT_STEMM  EXCEMPT_SYNON EXCEMPT_SHUFF UNIQUE_VALUES
+0                 mesaj içeriğini  message_start            NaN            NaN          True           NaN
+1  hadi gel köyümüze geri dönelim        message            NaN            NaN          True          True
+2                             yap     send_email            NaN            NaN           NaN           NaN 
+'''
+# class Intent:
+#     '''
+#         represents one datum
+#         the datum is represented as a dataframe
+#     '''
+#     def __init__(self,seq_in,seq_out,label,augmentation_settings_df):
+#         '''
+#             seq_in: input string
+#             seq_out: ground truth of slots
+#             label: ground truth value of the category
+#         '''
+#         seq_in_list, seq_out_list = group_words_into_slots(seq_in,seq_out)
+#         self.df = pd.DataFrame({"TOKEN":np.array(seq_in_list),"SLOT":np.array(seq_out_list)})
+#     def get_strings(self):
+#         '''
+#             returns seq_in, seq_out, label
+#         '''
+#         pass
+
+class Singleton(object):
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Singleton, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
 
 class AugmentableDataset:
     def __init__(self):
-        self.intents_dict = {}
-        self.augmentation_settings_dict = {}
+        self._intents_dict = {}
+        self._augmentation_settings_dict = get_augmentation_settings()
 
-    def add_to_dict(self,intent_name,intent_object):
-        if intent_name not in self.intents_dict:
-            self.intents_dict[intent_name] = [intent_object,]
+    def __copy__(self):
+        raise Exception('Should not be shallow copied')
+
+    def __deepcopy__(self, memo):
+        obj = AugmentableDataset()
+        obj._intents_dict = deepcopy(self._intents_dict, memo)
+        obj._augmentation_settings_dict = deepcopy(self._augmentation_settings_dict, memo)
+        return obj
+
+    def add_to_dict(self,intent_name,intent_df):
+        if intent_name not in self._intents_dict:
+            self._intents_dict[intent_name] = [intent_df,]
         else:
-            self.handle_unique_requirement(intent_object)
-            self.intents_dict[intent_name].append(intent_object)
-    @classmethod
-    def create_augmentation_settings_df(cls,augmentation_settings):
-        pass
+            self.handle_unique_requirement(intent_df, self._intents_dict[intent_name])
+            self._intents_dict[intent_name].append(intent_df)
+    
+    def fill_intents_dict(self):
+        unique_labels = IntentInstance.objects.values_list("label",flat=True).distinct()
+        unique_labels = [x for x in unique_labels]
+        unique_labels=["SendEmail"]
+        for intent_label in unique_labels:
+            '''
+                creating a dict of intent categories mapped to Intent objects
+            '''
+            intents_by_label = IntentInstance.objects.filter(label=intent_label)
+            augmentation_settings_df = self._augmentation_settings_dict[intent_label]
+            for intent in intents_by_label:
+                df = goo_to_dataframe(intent.seq_in,intent.seq_out)
+                '''
+                outer join augmentation settings
+                '''
+                df = pd.merge(df,augmentation_settings_df,on='SLOT',how='left')
+                self.add_to_dict(intent_label,df)
 
-    def handle_unique_requirement(self,intent_object):
+
+    def handle_unique_requirement(self,intent_df, rest_of_dfs):
         '''
-            makes sure slots specified in augmentation_settings have unique contents
+            checks if specified slot values are unique amongst other intents
         '''
         pass
-
-def get_augmentation_settings():
-    augmentation_settings = {} #intent_name settings_df pairs
-    intent_objects = IntentCategory.objects.all()
-
-    def get_slot_list(querylist):
-        return [x["slot_name"] for x in querylist]
-    for i in range(len(intent_objects)):
-        slots = intent_objects[i].intentslot_set
-        columns = {}  
-        columns["EXCEMPT_STEMM"] = get_slot_list(slots.filter(excempt_stemmify=True).values('slot_name'))
-        columns["EXCEMPT_SYNON"] = get_slot_list(slots.filter(excempt_synonym=True).values('slot_name'))
-        columns["EXCEMPT_SHUFF"]  = get_slot_list(slots.filter(excempt_shuffle=True).values('slot_name'))
-        columns["UNIQUE_VALUES"] = get_slot_list(slots.filter(unique_values_only=True).values('slot_name'))
-        
-        df_index = get_slot_list(slots.values('slot_name'))
-        df = pd.DataFrame(index=df_index)
-        df.index.name = "SLOT"
-        for (key, values) in columns.items():
-            column =  pd.Series(np.array([True for x in range(len(values))]),index=np.array(values))
-            df[key] = column
-        augmentation_settings[intent_objects[i].intent_label] = df
-    return augmentation_settings
 
 def tests():
+    from copy import copy
     import time
-    intents_dict = {}
-    intents_dict = {}
-    augmented_dict = {}
-    unique_labels = IntentInstance.objects.values_list("label",flat=True).distinct()
-    unique_labels = [x for x in unique_labels]
-    unique_labels=["SendEmail"]
-    # Get lates augmentation settings:
-    augmentation_settings = get_augmentation_settings()
-    # # Create intents_dict
-    for intent_label in unique_labels:
-        '''
-            creating a dict of intent categories mapped to Intent objects
-        '''
-        intents_by_label = IntentInstance.objects.filter(label=intent_label)
-        augmentation_settings_df = augmentation_settings[intent_label]
-        for intent in intents_by_label:
-            df = goo_to_dataframe(intent.seq_in,intent.seq_out)
+    original_dataset = AugmentableDataset()
+    original_dataset.fill_intents_dict()
+    cpy = deepcopy(original_dataset)
+    for key, x in original_dataset._intents_dict.items():
+        # original_dataset._intents_dict
+        
+        for y in x:
+            print(y,"\n")
+            break
+        break
+
+    # intents_dict = {}
+    # intents_dict = {}
+    # augmented_dict = {}
+    # unique_labels = IntentInstance.objects.values_list("label",flat=True).distinct()
+    # unique_labels = [x for x in unique_labels]
+    # unique_labels=["SendEmail"]
+    # # Get lates augmentation settings:
+    # augmentation_settings = get_augmentation_settings()
+    # # # Create intents_dict
+
+    # for intent_label in unique_labels:
+    #     '''
+    #         creating a dict of intent categories mapped to Intent objects
+    #     '''
+    #     intents_by_label = IntentInstance.objects.filter(label=intent_label)
+    #     augmentation_settings_df = augmentation_settings[intent_label]
+    #     for intent in intents_by_label:
+    #         df = goo_to_dataframe(intent.seq_in,intent.seq_out)
             
-            #outer join augmentation settings
-            df = pd.merge(df,augmentation_settings_df,on='SLOT',how='left')
+    #         #outer join augmentation settings
+    #         df = pd.merge(df,augmentation_settings_df,on='SLOT',how='left')
 
+    #         # # creating ignore lists
+    #         # slots = intent.seq_out.replace("B-","").replace("I-","").split()
+    #         # for slot in slots:
+    #         #     stemmify_ignorelist.append(slot in excempt_stemmify_tokens)
+    #         #     synonym_ignorelist.append(slot in excempt_synonym_tokens)
+    #         #     shuffle_ignorelist.append(slot in excempt_shuffle_tokens)
+    #         #     unique_values_only_list.append(slot in unique_values_only_tokens)
 
-            
-            # # creating ignore lists
-            # slots = intent.seq_out.replace("B-","").replace("I-","").split()
-            # for slot in slots:
-            #     stemmify_ignorelist.append(slot in excempt_stemmify_tokens)
-            #     synonym_ignorelist.append(slot in excempt_synonym_tokens)
-            #     shuffle_ignorelist.append(slot in excempt_shuffle_tokens)
-            #     unique_values_only_list.append(slot in unique_values_only_tokens)
+    #         # # Verifying list lengths
+    #         # lengths = map(len,[stemmify_ignorelist,synonym_ignorelist,shuffle_ignorelist,unique_values_only_list,intent.seq_in.split(),intent.seq_out.split()])
+    #         # if not len(set(lengths))==1:
+    #         #     raise Exception("All lists are not the same length!")
 
-            # # Verifying list lengths
-            # lengths = map(len,[stemmify_ignorelist,synonym_ignorelist,shuffle_ignorelist,unique_values_only_list,intent.seq_in.split(),intent.seq_out.split()])
-            # if not len(set(lengths))==1:
-            #     raise Exception("All lists are not the same length!")
-
-            # cls.add_to_dict(intents_dict, intent_label, (intent.seq_in,intent.seq_out,(stemmify_ignorelist,synonym_ignorelist, shuffle_ignorelist,unique_values_only_list)))
+    #         # cls.add_to_dict(intents_dict, intent_label, (intent.seq_in,intent.seq_out,(stemmify_ignorelist,synonym_ignorelist, shuffle_ignorelist,unique_values_only_list)))
--- mysite/submit_intents/dataset_augmentation/utils.py
+++ mysite/submit_intents/dataset_augmentation/utils.py
@@ -1,9 +1,6 @@
 import numpy as np 
 import pandas as pd 
-
-def create_augmentation_settings_df(augmentation_settings):
-    pass
-
+from ..models import IntentInstance, IntentCategory
 
 def goo_to_dataframe(seq_in,seq_out):
     seq_in_ = seq_in.split()
@@ -38,42 +35,50 @@ def dataframe_to_goo(dataframe):
     seq_out = " ".join(seq_out)
     return seq_in, seq_out
 
-def goo_format_(seq_in,seq_out):
+
+def get_augmentation_settings():
     '''
-        returns seq_in_list and seq_out_list 
-        where the lists contain token strings that are grouped words
+        returns a dictionary of augmentation setting dataframes
     '''
-    seq_in_ = seq_in.split()
-    seq_out_ = seq_out.split()
-    seq_in_list = [seq_in[0]]
-    seq_out_list = [seq_out[0]]
+    augmentation_settings = {}
+    intent_objects = IntentCategory.objects.all()
 
-    for i, (word, token) in enumerate(zip(seq_in_[1:],seq_out_[1:])):
-        if token.startswith("I-") and (seq_out_list[-1].startswith("B-") or seq_out_list[-1].startswith("I-")):
-            seq_in_list[-1] += " " + word
-            seq_out_list[-1] += " " + token
-        else:
-            seq_in_list.append(word)
-            seq_out_list.append(token)
+    def get_slot_list(querylist):
+        return [x["slot_name"] for x in querylist]
+    for i in range(len(intent_objects)):
+        slots = intent_objects[i].intentslot_set
+        columns = {}  
+        columns["EXCEMPT_STEMM"] = get_slot_list(slots.filter(excempt_stemmify=True).values('slot_name'))
+        columns["EXCEMPT_SYNON"] = get_slot_list(slots.filter(excempt_synonym=True).values('slot_name'))
+        columns["EXCEMPT_SHUFF"]  = get_slot_list(slots.filter(excempt_shuffle=True).values('slot_name'))
+        columns["UNIQUE_VALUES"] = get_slot_list(slots.filter(unique_values_only=True).values('slot_name'))
+        
+        df_index = get_slot_list(slots.values('slot_name'))
+        df = pd.DataFrame(index=df_index)
+        df.index.name = "SLOT"
+        for (key, values) in columns.items():
+            column =  pd.Series(np.array([True for x in range(len(values))]),index=np.array(values))
+            df[key] = column
+        augmentation_settings[intent_objects[i].intent_label] = df
+    return augmentation_settings
 
-    return seq_in_list, seq_out_list
 
-def group_words_into_slots(seq_in,seq_out):
-    '''
-        returns seq_in_list and seq_out_list 
-        where the lists contain token strings that are grouped words
-    '''
-    seq_in_ = seq_in.split()
-    seq_out_ = seq_out.split()
-    seq_in_list = [seq_in[0]]
-    seq_out_list = [seq_out[0]]
+# def group_words_into_slots(seq_in,seq_out):
+#     '''
+#         returns seq_in_list and seq_out_list 
+#         where the lists contain token strings that are grouped words
+#     '''
+#     seq_in_ = seq_in.split()
+#     seq_out_ = seq_out.split()
+#     seq_in_list = [seq_in[0]]
+#     seq_out_list = [seq_out[0]]
 
-    for i, (word, token) in enumerate(zip(seq_in_[1:],seq_out_[1:])):
-        if token.startswith("I-") and (seq_out_list[-1].startswith("B-") or seq_out_list[-1].startswith("I-")):
-            seq_in_list[-1] += " " + word
-            seq_out_list[-1] += " " + token
-        else:
-            seq_in_list.append(word)
-            seq_out_list.append(token)
+#     for i, (word, token) in enumerate(zip(seq_in_[1:],seq_out_[1:])):
+#         if token.startswith("I-") and (seq_out_list[-1].startswith("B-") or seq_out_list[-1].startswith("I-")):
+#             seq_in_list[-1] += " " + word
+#             seq_out_list[-1] += " " + token
+#         else:
+#             seq_in_list.append(word)
+#             seq_out_list.append(token)
 
-    return seq_in_list, seq_out_list
+#     return seq_in_list, seq_out_list
